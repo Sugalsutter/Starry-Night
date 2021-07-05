e5997f46ad839372f57f4ff2cd38e849130e63e8
DDuplantier <specialized.davidd@gmail.com>
2021-07-04 17:53:07 -0700

    Rename files

--- Util.py
+++ Util.py
@@ -2,14 +2,14 @@ import numpy as np
 from PIL import Image
 
 # Returns a numpy array representing an image.
-# Input: string filename of image
+# Input: string filename of image.
 def load_image(filename):
     with Image.open(filename) as img:
         return np.asarray(img)
 
 # Returns a numpy array of numpy arrays representing a
 # list of images.
-# Input: array of strings of filenames for images
+# Input: array of strings of filenames for images.
 def load_images(filenames):
     image_data = []
     for f in filenames:
--- Viz.py
+++ Viz.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 # Display image.
 # Input: string filename of image.
-def show_image(filename):
+def display_image(filename):
     with Image.open(filename) as img:
         plt.imshow(img)
         plt.show()
