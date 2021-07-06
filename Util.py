ecad388f42cee0e9e2c43f9b8085ffbda52082c3
DDuplantier <specialized.davidd@gmail.com>
2021-07-06 10:05:36 -0700

    Initialize Loader class for preparing batches of training data

--- Util.py
+++ Util.py
@@ -17,3 +17,24 @@ def load_images(filenames):
         with Image.open(f) as img:
             image_data.append(np.asarray(img))
     return np.asarray(image_data)
+
+# Class handles preparing batches of (test_data, target) pairs.
+class Loader():
+
+    # Constructor
+    #
+    # Input
+    # image_data (string): directory containing JPEG image files
+    # solutions_file (string): CSV file containing training data solutions
+    def __init__(self, image_dir, solutions_file):
+        self.image_dir = image_dir
+        self.solutions_file = solutions_files
+
+    # Returns a batch of (test_data, target) pairs of a given size.
+    #
+    # Input
+    # size (integer): the number of (test_data, target) pairs to return
+    #
+    # Returns array of (test_data, target) pairs.
+    def get_batch(size):
+        pass
